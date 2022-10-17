ec096b65ecb8d44a4cdebe59e1ae44acf2f4218e
Abhirup <ghoshabhirup2001@gmail.com>
2022-10-17 19:47:35 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+
+  useEffect(() => {
+    fetchAdvice();
+  });
+
+  const fetchAdvice = () => {
+    fetch("https://api.adviceslip.com/advice")
+      .then((data) => data.json())
+      .then((data) => {
+        setAdvice(data.slip.advice);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="card">
+        <h1 className="header">{advice}</h1>
+        <button onClick={fetchAdvice}>Give me an advice</button>
+      </div>
     </div>
   );
 }
